34c9dd0ac6bea4894519bc3c5d4b62cbf91f0cde
Allen <almcbroombey@hotmail.com>
2022-03-03 16:02:03 -0500

    psuedo coding js file

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,29 @@
 // Assignment Code 
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+   console.log("you pressed button");
+
+//WHEN prompted for password criteria
+//THEN I choose a length of at least 8 characters 
+var characters = "012345678910abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+?><:{}[]";
+var passwordLength = 128
+var password = "";
+for (let i = 0; i < passwordLength; i++) {
+var randomNumber = Math.floor(Math.random() * characters.length);
+password += characters.substring(randomNumber, randomNumber + 1);
+}
+ document.getElementById("password").value = password
+
+ return "Generated Password wil go here!";
+}
+
+//and no more than 128 characters  //and they are lc,uc, num., spc charac.
+//THEN my input should be validated and at least one character type should be selected
+//THEN a password is generated that matches the selected criteria
+//THEN the password is either displayed in an alert or written to the page
+  
+
 // Write password to the #password input only do generate password
 function writePassword() {
   var password = generatePassword();
@@ -13,5 +36,6 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+
 /* we need to generate a password */
-generatePassword
+console.log("generatePassword");
