55302b16150143ab26fdb7e2246fb6f5656e0b1b
Allen <almcbroombey@hotmail.com>
2022-03-05 23:45:29 -0500

    line 64 adding random list

--- Develop/script.js
+++ Develop/script.js
@@ -1,41 +1,87 @@
 // Assignment Code 
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword() {
-   console.log("you pressed button");
-
-//WHEN prompted for password criteria
-//THEN I choose a length of at least 8 characters 
-var characters = "012345678910abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+?><:{}[]";
-var passwordLength = 128
-var password = "";
-for (let i = 0; i < passwordLength; i++) {
-var randomNumber = Math.floor(Math.random() * characters.length);
-password += characters.substring(randomNumber, randomNumber + 1);
-}
- document.getElementById("password").value = password
-
- return "Generated Password wil go here!";
+function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min) + min)
 }
 
-//and no more than 128 characters  //and they are lc,uc, num., spc charac.
-//THEN my input should be validated and at least one character type should be selected
-//THEN a password is generated that matches the selected criteria
-//THEN the password is either displayed in an alert or written to the page
+function getRandomItem(list) {
+    return list[randomInt(0, list.length - 1)]
+}
   
 
-// Write password to the #password input only do generate password
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  function generatePassword() {
+    
+    var userInput = window.prompt('How long do you want your password to be?');
 
-  passwordText.value = password;
+    var passwordLength = parseInt(userInput)
+    
+    if (isNaN(passwordLength)){
+      window.alert('That is not a number!')
+      return
+    } 
+
+    if (passwordLength < 8 || passwordLength >128) {
+      window.alert('Password length must be between 8 and 128 characters')
+      return
+    }
+
+    var userneedsUppercase = window.confirm("Do you want to have uppercase letters in your password?? Ok - Yes Cancel - No")
+    var userneedsLowercase = window.confirm("Do you want to have lowercase letters in your password? Ok - Yes Cancel - No")
+    var userneedsNumbers = window.confirm("Do you want to have numbers in your password? Ok - Yes Cancel - No")
+    var userneedsSymbols = window.confirm("Do you want to have symbols in your password? Ok - Yes Cancel - No")
+
+    var uppercaseList = ['A', 'B', 'C' , 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    var lowercaseList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ]
+    var numbersList = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    var symbolsList = ['!', '@', '#', '$', '^', '*', '<', '?'] 
+
+    var allcharactersList = []
+
+    //consider a for loop for changing lowercase char. to uppercase characters.
+   
+    if (userneedsUppercase === true) (
+      allcharactersList.push(uppercaseList)
+    )
+
+    if (userneedsLowercase === true) (
+      allcharactersList.push(lowercaseList)
+    )
+
+    if (userneedsNumbers === true) (
+      allcharactersList.push(numbersList)
+    )
+
+    if (userneedsSymbols === true) (
+      allcharactersList.push(symbolsList)
+    ) 
+    
+     var newPassword = ''
+
+     for (var i = 0; i < passwordLength; i++) {
+       var randomOptionsList = getRandomItem(allcharactersList)//stopped here
+     }
+       var randomChar = getRandomItem(randomList)
+  }
+
+  
+
+
+
+// Write password to the #password input only do generate password
+  function writePassword() {
+   var password = generatePassword();
+   var passwordText = document.querySelector("#password");
+
+   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
 /* we need to generate a password */
-console.log("generatePassword");
+generatePassword 
+
+                                   //"add variable here
+//a for loop   for (var i = 0; i <  symbolsList[i].
